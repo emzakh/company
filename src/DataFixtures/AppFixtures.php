7df97cfde6f0ce3fc2dd3d40537400e6f765e0ff
emzakh <massimino.gutierrezmantione@gmail.com>
2021-02-08 18:03:39 +0100

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,57 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this ->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        
+        $adminUser = new User();
+        $adminUser -> setFirstName('Maximino')
+                ->setLastName('Gutierrez Mantione')
+                ->setEmail('emzakh@gmail.be')
+                ->setPassword($this -> passwordEncoder->encodePassword($adminUser,'password'))
+                ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($adminUser);
+   
+
+        $manager->persist($adminUser);
+
+       
+        for($c=0 ; $c < 10 ; $c++)
+        {
+            $category = new Category();
+            $category->setTitle($faker->word());
+            $manager->persist($category);
+
+          
+            for($p=0 ; $p < rand(2,8) ; $p++){
+                $product = new Product();
+                $product->setTitle($faker->sentence(rand(2,5), true))
+                    ->setPrice(rand(50,5000)/100)
+                    ->setDescription($faker->paragraph(4, true))
+                    ->setCategory($category)
+                ;
+                $manager->persist($product);
+            }
+        }
 
         $manager->flush();
     }
